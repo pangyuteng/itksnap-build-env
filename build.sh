fde4a81cb2b4111b1557b7cc4fc7466ba7203096
pangyuteng <pangyuteng@gmail.com>
2022-09-08 05:16:33 +0000

    wip

--- build.sh
+++ build.sh
@@ -27,17 +27,14 @@ docker build \
 # 3.8.0 and later	rel_3.8	2.8.12	4.12.2	6.3.0	Qt 5.6.0
 
 CMAKE_VER=3.22.6
-QT_MM_VER=5.6
-QT_VER=5.6.0
+QT_VER=5.6.3
 VTK_VER=7.1.1
 ITK_VER=4.13.2
 
 docker build \
     --build-arg CMAKE_VER=${CMAKE_VER} \
-    --build-arg QT_MM_VER=${QT_MM_VER} \
     --build-arg QT_VER=${QT_VER} \
     --build-arg VTK_VER=${VTK_VER} \
     --build-arg ITK_VER=${ITK_VER} \
     -f Dockerfile.qt5 \
     -t itksnap-${CMAKE_VER}-${QT_VER}-${VTK_VER}-${ITK_VER} .
-
