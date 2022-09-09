6a196d9079b84f5ea2f203b536fe59fb111a8383
pangyuteng <pangyuteng@gmail.com>
2022-09-09 04:53:11 +0000

    added ITKSNAP_TAG

--- build-qt5.sh
+++ build-qt5.sh
@@ -14,14 +14,14 @@ CMAKE_VER=3.22.6
 QT_VER=5.12.11
 VTK_VER=7.1.1
 ITK_VER=4.13.2
-ITKSNAP_VER=3.4.0
+ITKSNAP_TAG=3.4.0
 
 docker build \
     --build-arg CMAKE_VER=${CMAKE_VER} \
     --build-arg QT_VER=${QT_VER} \
     --build-arg VTK_VER=${VTK_VER} \
     --build-arg ITK_VER=${ITK_VER} \
-    --build-arg ITKSNAP_VER=${ITKSNAP_VER} \
+    --build-arg ITKSNAP_TAG=${ITKSNAP_TAG} \
     -f Dockerfile.qt5 \
-    -t itksnap-v${ITKSNAP_VER} .
-#    -t itksnap-${CMAKE_VER}-${QT_VER}-${VTK_VER}-${ITK_VER} .
+    -t itksnap-${ITKSNAP_TAG} .
+
